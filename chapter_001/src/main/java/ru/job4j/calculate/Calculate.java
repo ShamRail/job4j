a3ea3b8f48ac4181fd8e65657ae7b626740a6385
ShamRail <rail1999g102r@mail.ru>
2018-07-02 17:31:55 +0500

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate; 
  
-public class Calculate{ 
+/*
+	*Calculate
+	*
+	*@author Rail Shamsemuhametov
+*/
+ public class Calculate{ 
+	/*
+		*@param args - args.
+		*@return Hello world.
+	*/
 	public static void main(String[] args){ 
 		System.out.println("Hello World"); 
 	} 
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Rail Shamsemukhametov
+* @version $Id$
+* @since 2.07.2018
+*/
+package ru.job4j.calculate;
