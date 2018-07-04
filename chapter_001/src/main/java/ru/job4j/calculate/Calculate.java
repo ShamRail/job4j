45f398cf8e13a3f95cb7a9099d9b90ec5e26a4b0
ShamRail <rail1999g102r@mail.ru>
2018-07-04 18:05:12 +0500

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,10 +6,20 @@ package ru.job4j.calculate;
 	*@author Rail Shamsemuhametov
 */
  public class Calculate{ 
+	
+	/*
+		* Method echo.
+		* @param name Your name.
+		* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}		
 	/*
 		*@param args - args.
 		*@return Hello world.
 	*/
+	
 	public static void main(String[] args){ 
 		System.out.println("Hello World"); 
 	} 
