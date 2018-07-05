c08e960acaa7eb075286151ba7884445803d4337
ShamRail <rail1999g102r@mail.ru>
2018-07-05 19:54:48 +0500

    4.1. Глупый бот

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * DummyBot
+ * @author Rail Shamsemukhametov
+ * @version 1.0
+ * @since 05.07.2018
+ */
+
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
