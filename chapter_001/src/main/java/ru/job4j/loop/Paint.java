ccadd6802a57a1b0545c180d01bff0248bf3e0dd
ShamRail <rail1999g102r@mail.ru>
2018-07-06 21:53:38 +0500

    5.5. Рефакторинг кода

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,4 +1,5 @@
 package ru.job4j.loop;
+import java.util.function.BiPredicate;
 
 /**
  * Paint.
@@ -10,19 +11,62 @@ package ru.job4j.loop;
 public class Paint {
 
     /**
-     * pyramid.
-     * Метод , строящий пирамиду в псевдографике. weight - ширина пирамиды.
+     * rightTrl.
+     * Метод , строящий пирамиду в псевдографике справа.
+     * row - строка , column - столбец.
+     * @param height - высота пирамиды.
+     * @return рисунок пирамиды.
+     * */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * leftTrl.
+     * Метод , строящий пирамиду в псевдографике слева.
      * row - строка , column - столбец.
      * @param height - высота пирамиды.
      * @return рисунок пирамиды.
      * */
 
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+    /**
+     * pyramid.
+     * Метод , строящий пирамиду в псевдографике.
+     * row - строка , column - столбец.
+     * @param height - высота пирамиды.
+     * @return рисунок пирамиды.
+     * */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+    /**
+     * loopBy.
+     * Метод , строящий пирамиду в псевдографике.
+     * row - строка , column - столбец.
+     * @param  weight - ширина пирамиды
+     * @param height - высота пирамиды.
+     * @return рисунок пирамиды.
+     * */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -3,14 +3,39 @@ package ru.job4j.loop;
 import org.junit.Test;
 import org.junit.Assert;
 import org.hamcrest.core.Is;
+import java.util.StringJoiner;
 
 public class PaintTest {
     @Test
-    public void whenHeightThreeThen() {
+    public void whenPyramid4Right() {
         Paint paint = new Paint();
-        String result = paint.pyramid(3);
-        final String line = System.getProperty("line.separator");
-        String expected = String.format("  ^  \r\n ^^^ \r\n^^^^^\r\n", line, line, line);
-        Assert.assertThat(result, Is.is(expected));
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        Assert.assertThat(rst,
+                Is.is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        Assert.assertThat(rst,
+                Is.is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
     }
 }
