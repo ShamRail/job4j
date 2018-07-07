8c1b7314602161b3badba5f3cb8e4b66c8253187
ShamRail <rail1999g102r@mail.ru>
2018-07-07 23:03:33 +0500

    Крестики-нолики на JavaFX

---
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -0,0 +1,90 @@
+package ru.job4j.tictactoe;
+
+import java.util.function.Predicate;
+
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    /**
+     * isWinnerX.
+     * метод , проверящий выигрышные позиции по х.
+     * @return результат
+     * */
+
+    public boolean isWinnerX() {
+        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0);
+
+    }
+
+    /**
+     * isWinnerO.
+     * метод , проверящий выигрышные позиции по y.
+     * @return результат
+     * */
+
+    public boolean isWinnerO() {
+        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
+                || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1)
+                || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
+                || this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0);
+    }
+
+    /**
+     * hacGap.
+     * Метод , проверяющий есть ли позиции для ходов.
+     * @return результат.
+     * */
+
+    public boolean hasGap() {
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (table[i][j] != null) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * fillBy.
+     * метод , проверящий выигрышные позиции.
+     * @param predicate - Нолик или Крестик
+     * @param startX - стартовая позиция по х.
+     * @param startY - стартовая позиция по у.
+     * @param deltaX - смещение по х.
+     * @param deltaY - смещение по у.
+     * @return результат
+     * */
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+}
