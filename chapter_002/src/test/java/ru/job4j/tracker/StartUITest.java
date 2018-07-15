b2ffbf3b01e624b2d3ff1ac001776d604bfece96
ShamRail <rail1999g102r@mail.ru>
2018-07-15 22:50:40 +0500

    6. Доработать тесты в StartUITest проверяющие вывод данных консоль

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,9 +1,16 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
 import ru.job4j.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertThat;
 
 public class StartUITest {
     @Test
@@ -76,5 +83,49 @@ public class StartUITest {
             }
         }
         Assert.assertThat(tracker.findAll()[0].getComments()[2], Is.is("comm3"));
+    }
+
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.out.println("execute after method");
+        System.setOut(this.stdout);
+    }
+
+    @Test
+    public void whenShowItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("name1", "desc1"));
+        Item item2 = tracker.add(new Item("name2", "desc2"));
+        Item item3 = tracker.add(new Item("name3", "desc3"));
+        Input input = new StubInput(new String[]{"1", "7"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(this.out.toByteArray()), Is.is(this.toString(item1.getId(), item2.getId(), item3.getId())));
+
+    }
+    private String toString(String id1, String id2, String id3) {
+        String s = System.lineSeparator();
+        String menu = String.format("Меню.%sВыберите пункт меню :%s0.Добавить новую заявку%s1.Показать все заявки%s2.Редактировать заявку%s3.Удалить заявку%s4.Найти заявку по id%s5.Найти заявки по имени%s6.Добавить комментарий по id%s7.Выйти%s",
+                s, s, s, s, s, s, s, s, s, s);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(menu);
+        stringBuilder
+                .append(String.format("------------ Все заявки --------------%s", s))
+                .append(String.format("идентификатор : %s, имя : name1, описание заявки : desc1%s", id1, s))
+                .append(String.format("идентификатор : %s, имя : name2, описание заявки : desc2%s", id2, s))
+                .append(String.format("идентификатор : %s, имя : name3, описание заявки : desc3%s", id3, s));
+        stringBuilder.append(menu);
+        return stringBuilder.toString();
     }
 }
