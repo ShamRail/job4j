cfb94405231d045528d5bdafba08c2fa4eef7c97
ShamRail <rail1999g102r@mail.ru>
2018-07-18 19:49:53 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,15 +1,24 @@
 package ru.job4j.tracker;
 
-import java.util.Scanner;
-
 public class ValidateInput implements Input {
-    private Scanner scanner = new Scanner(System.in);
+
+    private final Input input;
+
+    public ValidateInput(final Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
     public int ask(String question, int[] range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = new ConsoleInput().ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Пожалуйста, выберите индекс внутри границ массива.");
@@ -18,10 +27,5 @@ public class ValidateInput implements Input {
             }
         } while (invalid);
         return value;
-    }
-
-    public String ask(String question) {
-        System.out.print(question);
-        return scanner.nextLine();
     }
 }
