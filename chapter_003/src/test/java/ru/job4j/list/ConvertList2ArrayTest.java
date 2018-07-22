d05f81d80ce8a8571e2cf17cca8e243838c5633c
ShamRail <rail1999g102r@mail.ru>
2018-07-22 14:51:00 +0500

    1. Конвертация листа массивов в один лист Integer

--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -1,8 +1,11 @@
 package ru.job4j.list;
 
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -33,5 +36,11 @@ public class ConvertList2ArrayTest {
                 {3, 4}
         };
         assertThat(result, is(expect));
+    }
+    @Test
+    public void whenConvertListOfArraysToOneList() {
+        ConvertList2Array convertlList = new ConvertList2Array();
+        List<int[]> list = Arrays.asList(new int[]{1, 2}, new int[]{3, 4, 5, 6}, new int[]{7, 8, 9});
+        Assert.assertThat(convertlList.convert(list), Is.is(Arrays.asList(1, 2, 3, 4 , 5 , 6, 7, 8, 9)));
     }
 }
