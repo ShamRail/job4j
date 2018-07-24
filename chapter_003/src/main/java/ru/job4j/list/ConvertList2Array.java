f8a3d409afb09c66a53d0f5f3c76b7b44aceef9f
ShamRail <rail1999g102r@mail.ru>
2018-07-24 15:34:28 +0500

    Исправления

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -10,14 +10,14 @@ public class ConvertList2Array {
             array[list.indexOf(value) / rows][list.indexOf(value) % rows] = value;
         }
         if (list.size() % rows != 0) {
-            for (int i = list.size(); i < rows*rows; i++) {
+            for (int i = list.size(); i < rows * rows; i++) {
                 array[i / rows][i % rows] = 0;
             }
         }
         return array;
     }
 
-    public List<Integer> convert (List<int[]> list) {
+    public List<Integer> convert(List<int[]> list) {
         List<Integer> result = new ArrayList<Integer>();
         for (int[] array : list) {
             for (int element : array) {
