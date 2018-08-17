60df2d66bd067cad7bf37f9b71e9589cc3d09f96
ShamRail <rail1999g102r@mail.ru>
2018-08-17 12:42:06 +0500

    Исправление задания (подсчет значения функций в диапозоне)

--- chapter_004/src/test/java/ru/job4j/lambda/FunctionsTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionsTest.java
@@ -3,23 +3,21 @@ package ru.job4j.lambda;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.List;
+import java.util.Arrays;
 
 public class FunctionsTest {
 
     @Test
     public void whenCheckLinearFucntion() {
         Functions functions = new Functions();
-        List<Double> result = functions.linearFunction(1, 3);
-        Assert.assertThat(result.get(0) + result.get(1) + result.get(2),
-                Is.is(6.0));
+        Assert.assertThat(functions.linearFunction(1, 3),
+                Is.is(Arrays.asList(1.0, 2.0, 3.0)));
     }
 
     @Test
     public void whenCheckSquareFunction() {
         Functions functions = new Functions();
-        List<Double> result = functions.squareFunction(1, 3);
-        Assert.assertThat(result.get(0) + result.get(1) + result.get(2),
-                Is.is(14.0));
+        Assert.assertThat(functions.squareFunction(1, 3),
+                Is.is(Arrays.asList(1.0, 4.0, 9.0)));
     }
 }
