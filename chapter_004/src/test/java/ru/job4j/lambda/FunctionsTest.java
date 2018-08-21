2cb1275e6aaea988763444464eda8a5a19525e8e
ShamRail <rail1999g102r@mail.ru>
2018-08-21 08:54:43 +0500

    Добавление теста к логарифмической функции

--- chapter_004/src/test/java/ru/job4j/lambda/FunctionsTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionsTest.java
@@ -20,4 +20,11 @@ public class FunctionsTest {
         Assert.assertThat(functions.squareFunction(1, 3),
                 Is.is(Arrays.asList(1.0, 4.0, 9.0)));
     }
+
+    @Test
+    public void whenCheckLogFunction() {
+        Functions functions = new Functions();
+        Assert.assertThat(functions.logarithmFunction(1, 3),
+                Is.is(Arrays.asList(0.0, 0.6931471805599453, 1.0986122886681098)));
+    }
 }
