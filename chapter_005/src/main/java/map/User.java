a9c2f19d4ccead6bc294f3638903704b5a0c59c8
ShamRail <rail1999g102r@mail.ru>
2018-08-30 15:09:30 +0500

    4. Переопределить только equals

--- chapter_005/src/main/java/map/User.java
+++ chapter_005/src/main/java/map/User.java
@@ -12,7 +12,7 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
-
+/*
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -20,6 +20,28 @@ public class User {
         result = prime * result + name.hashCode();
         result = prime * result + children;
         result = prime * result + birthday.hashCode();
+        return result;
+    }
+*/
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        boolean checked = false;
+        if (this == obj) {
+            result = true;
+            checked = true;
+        }
+
+        if (!checked && (obj == null || this.getClass() != obj.getClass())) {
+            checked = true;
+        }
+
+        if (!checked) {
+            User user = (User) obj;
+            result = (this.children == user.children)
+                    && (this.birthday.getTimeInMillis() == user.birthday.getTimeInMillis())
+                    && (this.name != null && user.name != null && this.name.equals(user.name));
+        }
         return result;
     }
 }
