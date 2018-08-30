a8ef43ec3c4519c54bef9b00d6f6b50c5300f7cc
ShamRail <rail1999g102r@mail.ru>
2018-08-30 14:19:41 +0500

    2. Не перекрывать equals hashCode

---
+++ chapter_005/src/test/java/map/UserTest.java
@@ -0,0 +1,30 @@
+package map;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserTest {
+
+    Map<User, Object> users = new HashMap<>();
+
+    @Before
+    public void setUp() {
+        User user1 = new User("Artem", 0, new GregorianCalendar(2000, Calendar.NOVEMBER, 1));
+        User user2 = new User("Artem", 0, new GregorianCalendar(2000, Calendar.NOVEMBER, 1));
+        users.put(user1, new Object());
+        users.put(user2, new Object());
+    }
+
+    @Test
+    public void whenEqualsAndHashodeUndefinedThenBothUsersWillBeInMap() {
+        System.out.println(users);
+        Assert.assertThat(users.size(), Is.is(2));
+    }
+}
