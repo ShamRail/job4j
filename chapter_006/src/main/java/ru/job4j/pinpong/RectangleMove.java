e0cd67d0dfd3486bc52a63d756bf01a0f7c38579
ShamRail <rail1999g102r@mail.ru>
2018-09-16 17:05:07 +0500

    1. Пин-понг.

---
+++ chapter_006/src/main/java/ru/job4j/pinpong/RectangleMove.java
@@ -0,0 +1,41 @@
+package ru.job4j.pinpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    private final int leftBorder = 0;
+    private final int rightBorder = 290;
+    private final int sleep = 50;
+    private final int shiftLeft = -1;
+    private final int shiftRight = 1;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+
+            int shift = 1;
+
+            if (this.rect.getX() == rightBorder) {
+                shift = shiftLeft;
+            }
+
+            if (this.rect.getX() == leftBorder) {
+                shift = shiftRight;
+            }
+
+            this.rect.setX(this.rect.getX() + shift);
+
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
