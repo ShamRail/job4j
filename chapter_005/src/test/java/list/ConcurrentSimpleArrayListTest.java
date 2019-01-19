238e2e55dc574e86b56196e15038f1fca24b17b4
ShamRail <rail1999g102r@mail.ru>
2019-01-19 16:04:42 +0500

    Correct test for concurrent ArrayList

--- chapter_005/src/test/java/list/ConcurrentSimpleArrayListTest.java
+++ chapter_005/src/test/java/list/ConcurrentSimpleArrayListTest.java
@@ -39,12 +39,6 @@ public class ConcurrentSimpleArrayListTest {
         thread1.join();
         thread2.join();
         Assert.assertThat(arrayList.size(), Is.is(5));
-        Assert.assertThat(arrayList.get(0), Is.is(1));
-        Assert.assertThat(arrayList.get(1), Is.is(2));
-        Assert.assertThat(arrayList.get(2), Is.is(3));
-        Assert.assertThat(arrayList.get(3), Is.is(4));
-        Assert.assertThat(arrayList.get(4), Is.is(5));
-        Assert.assertThat(arrayList.get(5), Is.is((Integer) null));
     }
 
     @Test
