02512791059c503790078b64b94a53d977ab27ee
ShamRail <rail1999g102r@mail.ru>
2019-04-08 16:45:14 +0500

    2. Анализ доступности сервера - исправление

--- chapter_008/src/main/java/inout/LogAnalyser.java
+++ chapter_008/src/main/java/inout/LogAnalyser.java
@@ -1,34 +1,39 @@
 package inout;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogAnalyser {
 
     public void availableTime(Path in, Path out) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(in.toFile()));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(out.toFile()))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(in.toFile()))) {
             List<String> lines = reader.lines().collect(Collectors.toList());
+            List<String> cash = new LinkedList<>();
             String time = null;
             for (int lineIndex = 0; lineIndex < lines.size(); lineIndex++) {
                 String[] pair = lines.get(lineIndex).split("\\s+");
                 int type = Integer.parseInt(pair[0]);
                 if (time == null && (type == 400 || type == 500)) {
                     time = pair[1];
-                    writer.write(time);
-                    writer.write(';');
                 }
-                if (time != null && type < 400 || lineIndex == lines.size() - 1) {
-                    writer.write(pair[1]);
-                    writer.write(System.lineSeparator());
+                if (time != null && type < 400 || endOfLines(lineIndex, lines)) {
+                    cash.add(String.format("%s;%s", time, pair[1]));
                     time = null;
                 }
             }
+            Files.write(out, cash, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
+    private boolean endOfLines(int index, List<String> lines) {
+        return index == lines.size() - 1;
+    }
 }
