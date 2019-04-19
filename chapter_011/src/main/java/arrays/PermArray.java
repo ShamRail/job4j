72d3e64c16cbb07a3479d9e5d09ba3cad48389a9
ShamRail <rail1999g102r@mail.ru>
2019-04-19 19:40:33 +0500

    2. Определить является ли одна строка перестановкой другой. [Исправление]

--- chapter_011/src/main/java/arrays/PermArray.java
+++ chapter_011/src/main/java/arrays/PermArray.java
@@ -5,18 +5,13 @@ public class PermArray {
     public boolean check(char[] word1, char[] word2) {
         boolean result = word1.length == word2.length;
         if (result) {
-            boolean[] marked = new boolean[word1.length];
-            int markedCount = 0;
+            long sumCodes1 = 0;
+            long sumCodes2 = 0;
             for (int i = 0; i < word1.length; i++) {
-                for (int j = 0; j < word2.length; j++) {
-                    if (word1[i] == word2[j] && !marked[i]) {
-                        marked[i] = true;
-                        markedCount++;
-                        break;
-                    }
-                }
+                sumCodes1 += Math.pow(Character.getNumericValue(word1[i]), 2);
+                sumCodes2 += Math.pow(Character.getNumericValue(word2[i]), 2);
             }
-            result = markedCount == marked.length;
+            result = sumCodes1 == sumCodes2;
         }
         return result;
     }
