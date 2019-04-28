c7cfd2edfb59a95cf40f509b9c3cb0dd65c407df
ShamRail <rail1999g102r@mail.ru>
2019-04-28 20:31:34 +0500

    1. Реализовать методы min max в стеке

--- chapter_011/src/test/java/stacks/PriorityStack.java
+++ chapter_011/src/test/java/stacks/PriorityStack.java
@@ -1,38 +1,73 @@
 package stacks;
 
 import java.util.*;
+import java.util.function.BiPredicate;
 
 public class PriorityStack<T> {
 
-    private LinkedList<T> stack = new LinkedList<>();
+    private Node<T> head = new Node<>(null);
+
+    private Node<T> tail = new Node<>(null);
 
     private Comparator<T> comparator;
 
     public PriorityStack(Comparator<T> comparator) {
         this.comparator = comparator;
+        this.head.next = tail;
+        this.tail.prev = head;
     }
 
     public void offer(T elem) {
-        if (stack.isEmpty()) {
-            stack.addFirst(elem);
-        } else {
-            T head = stack.pollFirst();
-            if (comparator.compare(head, elem) > 0) {
-                stack.addFirst(elem);
-                stack.addFirst(head);
-            } else {
-                stack.addFirst(head);
-                stack.addFirst(elem);
-            }
-        }
+        Node<T> node = new Node<>(elem);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
     }
 
     public T peek() {
-        return stack.peekFirst();
+        return head.next.data;
     }
 
     public T poll() {
-        return stack.pollFirst();
+        T result = head.next.data;
+        head.next = head.next.next;
+        head.next.prev = head;
+        return result;
+    }
+
+    public T min() {
+        return retrieve((el1, el2) -> comparator.compare(el1, el2) < 0);
+    }
+
+    public T max() {
+        return retrieve((el1, el2) -> comparator.compare(el1, el2) > 0);
+    }
+
+    private T retrieve(BiPredicate<T, T> condition) {
+        T result = null;
+        if (head.next != null) {
+            Node<T> current = head.next;
+            result = current.data;
+            while (current.next != null) {
+                if (condition.test(current.data, result)) {
+                    result = current.data;
+                }
+                current = current.next;
+            }
+        }
+        return result;
+    }
+
+    private static class Node<T> {
+        Node<T> prev;
+        Node<T> next;
+        T data;
+
+        Node(T data) {
+            this.data = data;
+        }
+
     }
 
 }
--- chapter_011/src/test/java/stacks/PriorityStackTest.java
+++ chapter_011/src/test/java/stacks/PriorityStackTest.java
@@ -29,4 +29,26 @@ public class PriorityStackTest {
         stack.offer(1);
         Assert.assertThat(stack.poll(), Is.is(1));
     }
+
+    @Test
+    public void maxTest() {
+        PriorityStack<Integer> stack = new PriorityStack<>(Integer::compareTo);
+        stack.offer(5);
+        stack.offer(4);
+        stack.offer(3);
+        stack.offer(2);
+        stack.offer(1);
+        Assert.assertThat(stack.max(), Is.is(5));
+    }
+
+    @Test
+    public void minTest() {
+        PriorityStack<Integer> stack = new PriorityStack<>(Integer::compareTo);
+        stack.offer(7);
+        stack.offer(2);
+        stack.offer(1);
+        stack.offer(4);
+        stack.offer(5);
+        Assert.assertThat(stack.min(), Is.is(1));
+    }
 }
